174c9f5b96a95b4d06a4b0bc04ec3668033f8426
BLM <Lifmon34rus@gmail.com>
2023-03-21 16:48:47 +0300

    Changed cascade type, so parent row would be intact. Fixed delete, added multiple roles, tweaks

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++
@@ -1,6 +0,0 @@
-package ru.kata.spring.boot_security.demo.service;
-
-import ru.kata.spring.boot_security.demo.repository.RoleRepository;
-
-public interface RoleService extends RoleRepository {
-}
--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import ru.kata.spring.boot_security.demo.models.Role;
-import ru.kata.spring.boot_security.demo.repository.RoleRepository;
-import ru.kata.spring.boot_security.demo.repository.UserRepository;
-
-
-
