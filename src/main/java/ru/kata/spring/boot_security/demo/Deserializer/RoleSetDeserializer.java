95bdafb20d962cec47ad75881cffc5e303769316
BLM <Lifmon34rus@gmail.com>
2023-03-31 10:40:28 +0300

    Testing modal for edit button

---
+++ src/main/java/ru/kata/spring/boot_security/demo/Deserializer/RoleSetDeserializer.java
@@ -0,0 +1,30 @@
+package ru.kata.spring.boot_security.demo.Deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.kata.spring.boot_security.demo.models.Role;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RoleSetDeserializer extends JsonDeserializer<Set<Role>> {
+    @Override
+    public Set<Role> deserialize(JsonParser jp, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException {
+        ObjectMapper mapper = (ObjectMapper) jp.getCodec();
+        JsonNode node = mapper.readTree(jp);
+        Set<Role> roles = new HashSet<>();
+        if (node.isArray()) {
+            for (JsonNode roleNode : node) {
+                roles.add(mapper.treeToValue(roleNode, Role.class));
+            }
+        }
+        return roles;
+    }
+}
+
